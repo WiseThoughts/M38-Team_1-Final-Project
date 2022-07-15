39d74dd645e80b4013abc925d362378562029828
WiseThoughts <benny.nebreda@gmail.com>
2022-07-15 20:56:07 +0100

    First Commit

---
+++ src/pages/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+
+
+
+import '../style/App.css';
+
+function App() {
+
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
