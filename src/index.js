0e3eff84774599226fe3fc599276e2716b0a5964
WiseThoughts <102035978+WiseThoughts@users.noreply.github.com>
2022-07-22 16:18:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,51 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from "./pages/app/App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./pages/App";
 import { BrowserRouter } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
-
-export const updateFetch = async (filterObj, updateObj, setter) => {
-  try {
-    const res = await fetch(`${process.env.REACT_APP_REST_API}user`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        filterObj,
-        updateObj,
-      }),
-    });
-    const data = await res.json();
-    if (data.msg !== "Successfully Updated") {
-      throw new Error(data.msg);
-    }
-    if (updateObj.username) {
-      setter(updateObj.username);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const deleteFetch = async (setter) => {
-  try {
-    const res = await fetch(`${process.env.REACT_APP_REST_API}user`, {
-      method: "DELETE",
-      headers: { Authorization: localStorage.getItem("myToken") },
-    });
-    const data = await res.json();
-    if (data.msg !== "Successfully Deleted") {
-      throw new Error(data.msg);
-    } else {
-      setter();
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
