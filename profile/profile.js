f692c1be328a06d00b43df72a6b4f4428f9f0154
WiseThoughts <102035978+WiseThoughts@users.noreply.github.com>
2022-07-24 01:40:23 +0100

    Add files via upload

---
+++ profile/profile.js
@@ -0,0 +1,45 @@
+import { ProfileTitle, ProfilePic, ProfileBacking  } from "./profile.styled";
+import Nav from "../../components/nav/nav"
+import Tabs from '../../components/userDetails/tabs';
+// import StarRating from "../../components/userDetails/starRating";
+
+import "../profile/profile.css"
+
+
+const Profile = ({profilePic, setter, user}) => {
+
+
+
+    return (
+    <div>
+
+        <div>
+            <Nav />
+        </div>
+
+        <div className="backgroundProfile profileSpacer"> 
+        <ProfileBacking className="postioning">
+        <ProfileTitle>{user}'s Profile</ProfileTitle>
+
+        <div  className="profilePic">
+            <ProfilePic src={profilePic} alt="users profile pic" />
+        </div>
+
+        <div className="ratingBox">
+            <h2>Your seller Rating:</h2>
+            <p>⭐⭐⭐⭐⭐</p>
+        </div>
+
+        <div>
+            <Tabs user={user} setter={setter} />
+        </div>
+
+        
+        </ProfileBacking>
+        </div>
+
+        </div>
+    );
+};
+
+export default Profile;
