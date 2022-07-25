10dc4a0c0738c1f481d66a0a6bb5f3bf7eb4e797
burn-Thor <eothorburn@gmail.com>
2022-07-25 17:14:34 +0100

    Monday

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
 import { Wrapper, NavbarItems, Logo  } from "./nav.styled";
-import "./nav.css"
-import logo from "../images/logomockupblank.png"
+import "./nav.css";
+import logo from "../images/logomockupblank.png";
+import {BsCart4} from 'react-icons/bs'
+
 
 
 const Nav = () => {
@@ -15,6 +17,7 @@ const Nav = () => {
         <Link className="navBarLinks" to="/profile"><NavbarItems>Profile</NavbarItems></Link>
         <Link className="navBarLinks" to="/shopPage"><NavbarItems>Shop</NavbarItems></Link>
         <Link className="navBarLinks" to="/about"><NavbarItems>About</NavbarItems></Link>
+        <Link className="navBarLinks" to="/cart"><NavbarItems><BsCart4/></NavbarItems></Link>
     </Wrapper>
 );
 };
