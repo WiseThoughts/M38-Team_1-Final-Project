7bfdf5baa39db11d32337473190b3bfc8fd2794e
EvoFox <foulkes.joshua@live.co.uk>
2022-07-26 14:04:38 +0100

    fixed a goof

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,38 +1,43 @@
-import { useState } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { faker } from '@faker-js/faker';
-import Login from '../../components/login/login';
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { faker } from "@faker-js/faker";
+import Login from "../../components/login/login";
 import Profile from "../profile/profile";
-import ShopPage from '../shop/shopPage';
+import ShopPage from "../shop/shopPage";
 import About from "../about/about";
 
 // import Listing from '../../components/createListing/createListing';
-import ListPage from './listings/listings';
+import ListPage from "./listings/listings";
 
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
+	const [user, setUser] = useState();
+	const profilePic = faker.image.avatar();
 
-  const [user, setUser] = useState();
-  const profilePic = faker.image.avatar();
-
-
-  return (
-  <div>
-    {!user && <Navigate to="/" />}
-    <Routes>
-        <Route path="/" element={<Login user={user} setter={setUser} />} />
-        <Route path="/profile" element={<Profile user={user} setter={setUser} profilePic={profilePic} />} />
-        <Route path="/shopPage" element={<ShopPage user={user} setter={setUser}/>} />
-        <Route path="/about" element={<About />} />
-        <Route path="/listings" element={<ListPage user={user} setter={setUser}  />}/>
-    </Routes>
-
-
-  </div>
-  );
+	return (
+		<div>
+			{/* {!user && <Navigate to="/" />} */}
+			<Routes>
+				<Route path="/" element={<Login user={user} setter={setUser} />} />
+				<Route
+					path="/profile"
+					element={
+						<Profile user={user} setter={setUser} profilePic={profilePic} />
+					}
+				/>
+				<Route
+					path="/shopPage"
+					element={<ShopPage user={user} setter={setUser} />}
+				/>
+				<Route path="/about" element={<About />} />
+				<Route
+					path="/listings"
+					element={<ListPage user={user} setter={setUser} />}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
